e163787ee31db5f8c40729f67f1bbf9134c4eadd
Victor Ciolac <victor.bcfraga@gmail.com>
2023-04-14 02:51:32 -0300

    organizando a pasta

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/ex-bonus1.js
@@ -0,0 +1,27 @@
+let lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+let lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+let lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+//Crie uma função para adicionar o turno da noite na lesson2. Essa função deve ter três parâmetros: o objeto a ser modificado, a chave a ser adicionada e o valor dela.
+
+function addTurn(obj, key, value) {
+  obj[key] = value;
+}
+addTurn(lesson2, 'turno', 'noite');
+console.log(lesson2);
